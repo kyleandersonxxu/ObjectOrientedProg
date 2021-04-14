9b74c6e76de23ce35b81ebbefcec075905092d31
Kyle Anderson <kyleandersonxxu@gmail.com>
2021-04-14 08:26:05 -0600

    added main repo

--- Students.py
+++ Students.py
@@ -15,11 +15,11 @@ class Student:
 
         def setGrade(self, grade):
             self.student_grade = grade
-            passingClass()
-            
+            self.passingClass()
+
     def extraCredit(self, points):
         self.grade = self.grade + points
-        passingClass()
+        self.passingClass()
 
     def passingClass(self):
         if self.grade < self.passing_grade:
@@ -27,32 +27,4 @@ class Student:
          else:
             self.award_credit = True
 
-
-W01234 = Student('Kyle', 'Anderson') #instance of Student Class
-W01235 = Student('Willy','Wanka')
-
-print('Start of the semester')
-print('-----------------------')
-
-W01234.extraCredit(30)
-
-print(W01234.passing_grade)
-print(W01235.first, W01235.last, W01235.email, W01235.status)
-
-
-print(W01234)
-print(W01235)
-
-W01234.first = "Kyle"
-W01234.last = "Anderson"
-W01234.Email = "masterblaste@mail.weber.edu"
-W01234.status = "Pass"
-
-W01235.first = "Willy"
-W01235.last = "Wonka"
-W01235.email = "kidsoup@mail.weber.edu"
-W01235.status = "Pass"
-
-# print(W01234.email)
-# print(W01235.email)
 
---
+++ main.py
@@ -0,0 +1,33 @@
+from Students import Student
+
+
+def main ()
+
+W01234 = Student('Kyle', 'Anderson') #instance of Student Class
+W01235 = Student('Willy','Wanka')
+
+print('Start of the semester')
+print('-----------------------')
+
+W01234.extraCredit(30)
+
+print(W01234.passing_grade)
+print(W01235.first, W01235.last, W01235.email, W01235.status)
+
+
+print(W01234)
+print(W01235)
+
+W01234.first = "Kyle"
+W01234.last = "Anderson"
+W01234.Email = "masterblaste@mail.weber.edu"
+W01234.status = "Pass"
+
+W01235.first = "Willy"
+W01235.last = "Wonka"
+W01235.email = "kidsoup@mail.weber.edu"
+W01235.status = "Pass"
+
+# print(W01234.email)
+# print(W01235.email)
+
